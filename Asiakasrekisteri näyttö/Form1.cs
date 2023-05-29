3b8fff08e1b3e97f0fa2edf8796dcb8c93922379
joonisRte66 <joonis.oksanen@edu.keuda.fi>
2023-05-29 11:30:43 +0300

    Koodiin lisätty luokka

--- Asiakasrekisteri näyttö/Form1.cs
+++ Asiakasrekisteri näyttö/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace Asiakasrekisteri_näyttö
 {
+    public class Asiakas
+    {
+        
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
