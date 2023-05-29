c91ebcd953a6a8d08f5fddb779f66354419c0531
Neondragon39 <vlad.jeskin@gmail.com>
2023-05-29 11:16:53 +0300

    NYT PERKELE TOIMIII

--- Asiakasrekisteri näyttö/Form1.Designer.cs
+++ Asiakasrekisteri näyttö/Form1.Designer.cs
@@ -28,13 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LisääYrBT = new System.Windows.Forms.Button();
+            this.PoistaYrBT = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.LisääYrTB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(896, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asiakasrekisteri";
+            // 
+            // LisääYrBT
+            // 
+            this.LisääYrBT.Location = new System.Drawing.Point(389, 572);
+            this.LisääYrBT.Name = "LisääYrBT";
+            this.LisääYrBT.Size = new System.Drawing.Size(165, 23);
+            this.LisääYrBT.TabIndex = 2;
+            this.LisääYrBT.Text = "Lisää";
+            this.LisääYrBT.UseVisualStyleBackColor = true;
+            // 
+            // PoistaYrBT
+            // 
+            this.PoistaYrBT.Location = new System.Drawing.Point(590, 572);
+            this.PoistaYrBT.Name = "PoistaYrBT";
+            this.PoistaYrBT.Size = new System.Drawing.Size(165, 23);
+            this.PoistaYrBT.TabIndex = 3;
+            this.PoistaYrBT.Text = "Poista";
+            this.PoistaYrBT.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(916, 572);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(217, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1276, 572);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(171, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Slogan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tähän lgo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(58, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(706, 417);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(916, 111);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(702, 417);
+            this.dataGridView2.TabIndex = 14;
+            // 
+            // LisääYrTB
+            // 
+            this.LisääYrTB.Location = new System.Drawing.Point(58, 550);
+            this.LisääYrTB.Multiline = true;
+            this.LisääYrTB.Name = "LisääYrTB";
+            this.LisääYrTB.Size = new System.Drawing.Size(291, 45);
+            this.LisääYrTB.TabIndex = 15;
+            this.LisääYrTB.Text = "Yrityksien nimi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1729, 635);
+            this.Controls.Add(this.LisääYrTB);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.PoistaYrBT);
+            this.Controls.Add(this.LisääYrBT);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LisääYrBT;
+        private System.Windows.Forms.Button PoistaYrBT;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox LisääYrTB;
     }
 }
 
